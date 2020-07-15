b275e5c5c3b7a8c290d0dc26788b621c6b93e02c
Tae Min Ha <taemin410@gmail.com>
2020-07-15 18:37:48 +0900

    train/test model, initialize pytest

--- main.py
+++ main.py
@@ -8,31 +8,61 @@ from src.model import NN_Model
 import argparse
 import torch
 
+
 def main(args):
 
-    # Load configurations 
-    configs = load_config('config.yml')
+    # Load configurations
+    configs = load_config("config.yml")
     modelConfig = configs["model"]
     trainConfig = configs["train"]
 
     # Initialize SummaryWriter for tensorboard
     # writer = SummaryWriter()
-    
+
     # Preprocess the data
     wind_dataset = final_dataset()
 
-    loader = DataLoader(wind_dataset, batch_size=8, shuffle=False, sampler=None,
-           batch_sampler=None, num_workers=0, collate_fn=None,
-           pin_memory=False, drop_last=False, timeout=0,
-           worker_init_fn=None)
-           
+    loader = DataLoader(
+        wind_dataset,
+        batch_size=8,
+        shuffle=False,
+        sampler=None,
+        batch_sampler=None,
+        num_workers=0,
+        collate_fn=None,
+        pin_memory=False,
+        drop_last=False,
+        timeout=0,
+        worker_init_fn=None,
+    )
 
-    input_dim = wind_dataset[0:1][0].shape[1]
-    # initialize Model 
-    model = NN_Model(input_dim=input_dim, output_dim=1, hidden_layers=modelConfig["hiddenlayers"])
+    n = 300
+    input_dim = 300
+    import numpy as np
 
-    model.train(loader)
-    # pickle_save()
+    x = np.random.standard_normal(size=(n, input_dim))
+    y = np.random.randint(low=0, high=2, size=(n, 1))
+
+    # input_dim = wind_dataset[0:1][0].shape[1]
+
+    data = torch.utils.data.TensorDataset(
+        torch.tensor(x, dtype=torch.float), torch.tensor(y, dtype=torch.float)
+    )
+
+    loader = torch.utils.data.DataLoader(data, batch_size=8)
+
+    # initialize Model
+    model = NN_Model(
+        input_dim=input_dim, output_dim=1, hidden_layers=modelConfig["hiddenlayers"]
+    )
+
+    model.train(loader, epochs=20)
+
+    testX, testY = loader.dataset[1:100]
+    print(testX.shape, testY.shape)
+
+    rmse = model.test(testX, testY)
+    print("RMSE:  ", rmse)
 
     # Evaluation phase
     # output = model.eval()
@@ -42,15 +72,14 @@ def main(args):
     # visualize(output)
 
 
-
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description="ai4impact project")
-    
-    parser.add_argument("--mode", type=str, default='main')
+
+    parser.add_argument("--mode", type=str, default="main")
 
     args = parser.parse_args()
 
-    args.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    args.device = "cuda" if torch.cuda.is_available() else "cpu"
 
     globals()[args.mode](args)
--- src/model.py
+++ src/model.py
@@ -1,16 +1,17 @@
 import torch
 import torch.nn as nn
-from torch.nn import functional as F  
+from torch.nn import functional as F
+from torch.utils.data import dataloader
+
 
 class NN_Model(nn.Module):
-
     def __init__(self, input_dim, output_dim, hidden_layers):
         super().__init__()
-        
+
         # Store input and output dimensions to instance var
         self.input_dim = input_dim
         self.output_dim = output_dim
-        
+
         current_input_dim = self.input_dim
 
         self.layers = nn.ModuleList()
@@ -19,49 +20,59 @@ class NN_Model(nn.Module):
             current_input_dim = hidden_dim
         # add the last layer
         self.layers.append(nn.Linear(current_input_dim, self.output_dim))
-        
+
     def forward(self, x):
         for layer in self.layers[:-1]:
             x = F.relu(layer(x))
         out = self.layers[-1](x)
 
         return out
- 
+
     def train(self, dataloader, epochs=10, batch_size=8, lr=0.001, writer=None):
 
-        # Initialize loss function and optimizer 
-        criterion = torch.nn.MSELoss()    # mean-squared error for regression
+        # Initialize loss function and optimizer
+        criterion = torch.nn.MSELoss()  # mean-squared error for regression
         optimizer = torch.optim.Adam(self.parameters(), lr=lr)
-        
+
         losses = []
-        val_losses = [] 
-        
+        val_losses = []
+
         # Train the model by epochs
         for epoch in range(epochs):
-            loss_sum = 0 
+            loss_sum = 0
 
             for xx, yy in dataloader:
-                print(xx)
 
-                print("yy: ", yy)
-            #     optimizer.zero_grad()
-            #     outputs = self(dataloader[i*batch_size: (i+1)* batch_size][0]).squeeze(1)
+                optimizer.zero_grad()
+                outputs = self(xx).squeeze(1)
 
-            #     # obtain the loss function
-            #     # TODO: find train_y from dataloader \
-            #     # TODO: Add tensorboard write 
-            #     loss = criterion(outputs, dataloader[i*batch_size: (i+1)* batch_size][1])
-            #     loss_sum += loss.clone()
-            #     loss.backward()
-            #     optimizer.step()
-            # losses.append(loss_sum)
+                # # obtain the loss function
+                # # TODO: find train_y from dataloader \
+                # # TODO: Add tensorboard write
+                # print(outputs.shape, yy.shape)
 
-            # # with torch.no_grad():
-            # #     outputs = self(val_x).squeeze(1)
-            # #     # val_loss = (torch.sum((outputs - val_y)**2) / outputs.shape[0])**0.5
-            # #     val_loss = criterion(outputs, val_y)
-            # #     val_losses.append(val_loss)
-                
-            # if epoch % 5 == 0:
-            #     print("Epoch: %d, loss: %1.5f" % (epochs, loss_sum))
+                loss = criterion(outputs, yy.squeeze(1))
+                loss_sum += loss.clone()
+                loss.backward()
+                optimizer.step()
 
+            losses.append(loss_sum)
+            with torch.no_grad():
+                outputs = self(dataloader.dataset[0:100][0]).squeeze(1)
+                # val_loss = (torch.sum((outputs - val_y)**2) / outputs.shape[0])**0.5
+                val_loss = criterion(outputs, dataloader.dataset[0:100][1].squeeze(1))
+                val_losses.append(val_loss)
+
+            if epoch % 5 == 0:
+                print("Epoch: %d, loss: %1.5f" % (epoch, loss_sum))
+
+        print("---train finished---")
+
+    def test(self, testX, ytrue):
+
+        ypred = self(testX).squeeze(1)
+        result = (ytrue - ypred) ** 2  # squared error
+
+        rmse = (torch.sum(result) / result.shape[0]) ** 0.5  # root mean squared error
+
+        return (rmse, ypred)
