80a42c07a6211eddc9e283b01e6dc31b507a2fb4
choijaewon <choijaewon959@gmail.com>
2020-07-20 17:08:57 +0800

    submission interval specified

--- src/request/request.py
+++ src/request/request.py
@@ -1,12 +1,11 @@
 import requests
+import threading
 
 PASSWORD = 1051579459
 URL = "http://3.1.52.222/submit/pred"
+RESUBMISSION_TIME_INTERVAL = 3600
 
 def submit_answer(pred_val):
-    """
-    @TODO: send this hourly OR depending on the input to the model -> should be confirmed after reaing the description
-    """
     payload = {
                 'pwd': PASSWORD,
                 'value': pred_val
@@ -16,6 +15,11 @@ def submit_answer(pred_val):
 
     print("response", res.text)
 
+def run_submission_session():
+    threading.Timer(RESUBMISSION_TIME_INTERVAL, run_submission_session).start()
+    
+    #@TODO: get pred val from somewhere else...
+    pred_val = 10
+    submit_answer(pred_val)
 
-
-submit_answer(100)
+run_submission_session()
