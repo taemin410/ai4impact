c25aee2a91818217bc01567d095d81f3d2f885e4
choijaewon <choijaewon959@gmail.com>
2020-07-24 18:27:59 +0800

    bias added

--- src/request/request.py
+++ src/request/request.py
@@ -1,7 +1,9 @@
 import requests
+import csv
 
 PASSWORD = 1051579459
-URL = "http://3.1.52.222/submit/pred"
+SUBMISSION_URL = "http://3.1.52.222/submit/pred"
+RESULT_URL = "https://ai4impact.org/p003-log.csv"
 
 def submit_answer(pred_val):
     payload = {
@@ -9,6 +11,22 @@ def submit_answer(pred_val):
                 'value': pred_val
             }
 
-    res = requests.post(URL, data = payload)
+    res = requests.post(SUBMISSION_URL, data = payload)
 
     print("response", res.text)
+
+def get_result_from_web():
+    with requests.Session() as s:
+        try:
+            download = s.get(RESULT_URL)
+
+            decoded_content = download.content.decode('utf-8')
+            cr = csv.reader(decoded_content.splitlines(), delimiter=',')
+
+            print("Successfully downloaded result from web!")
+            return cr
+
+        except Exception as err:
+            print("Error in getting result from web: ", err)
+            return None
+        
